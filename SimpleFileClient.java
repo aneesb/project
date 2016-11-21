7de3437c23f8eda7a693e0d35c236fff3f31f2fc
aneesb <anees.bishara@gmail.com>
2016-11-22 00:40:30 +0200

    Update SimpleFileClient.java

--- SimpleFileClient.java
+++ SimpleFileClient.java
@@ -1,10 +1,9 @@
-package servers;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
-
+import java.nio.*;
 
 
 public class SimpleFileClient {
@@ -34,16 +33,34 @@ public class SimpleFileClient {
       InputStream is = sock.getInputStream();
       fos = new FileOutputStream(FILE_TO_RECEIVED);
       bos = new BufferedOutputStream(fos);
-      bytesRead = is.read(mybytearray,0,mybytearray.length);
+      // bytesRead = is.read(mybytearray,0,mybytearray.length);
+      //  current = bytesRead;
+      byte[] array = new byte[16];
+      bytesRead = is.read(array,0,array.length);
       current = bytesRead;
 
+      System.out.println("STARTED READING");
+      
       do {
+	  if(bytesRead == array.length) break;
          bytesRead =
-            is.read(mybytearray, current, (mybytearray.length-current));
-         if(bytesRead >= 0) current += bytesRead;
+	     // is.read(mybytearray, current, (mybytearray.length-current));
+	  is.read(array, current, (array.length-current));
+	if(bytesRead >= 0) current += bytesRead;
       } while(bytesRead > -1);
-      System.out.println("IM OUT!");
-      bos.write(mybytearray, 0 , current);
+       System.out.println("FINISHED READING");
+      //bos.write(mybytearray, 0 , current);
+      IntBuffer intBuf =
+        		   ByteBuffer.wrap(array)
+        		     .order(ByteOrder.BIG_ENDIAN)
+        		     .asIntBuffer();
+        		 int[] array2 = new int[intBuf.remaining()];
+        		 intBuf.get(array2);
+        		 for (int i=0; i < array2.length; i++)
+        	        {
+        	            System.out.println(i + ": " + array2[i]);
+        	        }
+
       bos.flush();
       System.out.println("File " + FILE_TO_RECEIVED
           + " downloaded (" + current + " bytes read)");
